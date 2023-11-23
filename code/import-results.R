1bfb585a28797f0dbceba277bde5f77451d715c3
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-11-23 13:48:13 +0000

    automatically switch between local/remote data

--- code/import-results.R
+++ code/import-results.R
@@ -13,13 +13,13 @@ library(curl)
 library(readr)
 
 # Get observed data
-import_observed <- function(local = TRUE) {
+import_observed <- function(update = FALSE) {
 
-  if (local) { # load a local copy
-    obs <- read_csv(here("data", "obs.csv"))
-
-  } else {
-
+  file_dir <- here("data")
+  dir.create(file_dir, showWarnings = FALSE) ## create if does not exist
+  file_path <- file.path(file_dir, "obs.csv")
+  local <- file.exists(file_path)
+  if (update || !local) { # need to generate local copy
     # get raw JHU data
     cases <- read_csv("https://raw.githubusercontent.com/covid19-forecast-hub-europe/covid19-forecast-hub-europe/main/data-truth/JHU/truth_JHU-Incident%20Cases.csv") |>
       mutate(target_variable = "inc case")
@@ -40,99 +40,49 @@ import_observed <- function(local = TRUE) {
       select(-year, -week) |>
       left_join(pop) |>
       mutate(model = "Observed")
-    write_csv(obs, here("data", "obs.csv"))
+    write_csv(obs, file_path)
     message("Data updated and saved locally")
   }
 
+  obs <- read_csv(file_path)
   return(obs)
 }
 
 # Get and format projections from local repo -----
 import_projections <- function(round = 2,
-                           local = TRUE,
+                           update = FALSE,
                            n_model_min = 3) {
 
-  if (local) {
-    # if hub is cloned locally, get path to hub/data-processed
-    hub_data_path <- gsub("aggregation-info-loss", # this dir
-                          "covid19-scenario-hub-europe", # hub dir
-                          here::here("data-processed"))
-
-    # get scenarios metadata
-    scenarios <- read_rds(here("data", "scenarios.rds"))
-
-    # get csv paths
-    model_results <- list.files(hub_data_path,
-                                full.names = TRUE, recursive = TRUE)
-    model_results <- model_results[grepl(".csv", model_results)]
-
-    # get model date and name
-    models <- tibble(file = model_results) |>
-      mutate(date = as.Date(str_extract(file,
-                                        "2022-[:digit:][:digit:]-[:digit:][:digit:]")),
-             model = str_remove(file, hub_data_path),
-             model = str_extract(model, "\\/(.+)\\/"),
-             model = str_remove_all(model, "\\/"))
-
-    # keep only files within round specific dates
-    origin_date <- scenarios[[paste0("round-", round)]][["origin_date"]]
-    window_end <- scenarios[[paste0("round-", round)]][["submission_window_end"]]
-    valid_dates <- seq.Date(from = as.Date(origin_date),
-                            to = as.Date(window_end), by = 1)
-    models <- filter(models, date %in% valid_dates)
-
-    # read csv
-    safely_read <- safely(~ read_csv(., show_col_types = FALSE))
-    results <- map(.x = models$file,
-                   ~ safely_read(.x))
-    names(results) <- models$model
-    results <- transpose(results)
-    results <- bind_rows(results$result, .id = "model") |>
-      mutate(round = round)
-
-    # add dates
-    results <- results |>
-      mutate(horizon = as.numeric(str_remove(horizon, " wk")),
-             target_end_date = origin_date + lubridate::weeks(horizon) - 1)
-
-    # models:
-    #   In round 1, USC submitted 2 sets of results from the same model
-    #    ("USC-SIkJalpha"), using only new data ~3 weeks apart ("USC-SIkJalpha_update").
-    #    Note the parquet version only includes the first of these.
-    #    If loading from local, use only "USC-SIkJalpha"
-    if (round == 1) {
-      models <- distinct(results, model) |>
-        filter(grepl("^USC-SIkJalpha", model))
-      if (nrow(models) > 1) {
-        results <- filter(results,
-                          !grepl("^USC-SIkJalpha_update$", model))
-      }
-    }
-
-  } else { # Get from remote source -----
-
+  file_dir <- here("data")
+  dir.create(file_dir, showWarnings = FALSE) ## create if does not exist
+  scenarios_file <- file.path(file_dir, "scenarios.rds")
+  data_name <- paste0("round", round, ".parquet")
+  data_file <- file.path(file_dir, data_name)
+  if (!file.exists(scenarios_file) || update) {
     # get scenario metadata
-    try(source("https://raw.githubusercontent.com/covid19-forecast-hub-europe/covid19-scenario-hub-europe-website/main/code/load/scenarios.R"))
-
+    source("https://raw.githubusercontent.com/covid19-forecast-hub-europe/covid19-scenario-hub-europe-website/main/code/load/scenarios.R")
+    saveRDS(scenarios, scenarios_file)
+  }
+  if (!file.exists(data_file) || update) {
     # get round path
-    data_path <- tempfile(fileext = ".parquet")
     url <- paste0("https://github.com/covid19-forecast-hub-europe/covid19-scenario-hub-europe/releases/download/round",
-                  round, "/round", round,
-                  ".parquet")
+                  round, "/", data_name)
 
     # Load from parquet storage
-    try(curl::curl_download(url, data_path))
-    results <- try(arrow::read_parquet(data_path))
-
-    # results loaded from parquet seem to have mixed target end dates
-    results <- results |>
-      mutate(horizon = as.numeric(substr(horizon, 1, 2)),
-             target_end_date = as.Date(scenarios[[paste0("round-", !!round)]][["origin_date"]]) +
-               lubridate::weeks(horizon) - lubridate::days(1),
-             round = round)
-    }
+    curl::curl_download(url, data_file)
+  }
+  scenarios <- readRDS(scenarios_file)
 
   # Formatting results data ------------------------------------------------
+  results <- try(arrow::read_parquet(data_file))
+
+  # results loaded from parquet seem to have mixed target end dates
+  results <- results |>
+    mutate(horizon = as.numeric(substr(horizon, 1, 2)),
+           target_end_date = as.Date(scenarios[[paste0("round-", !!round)]][["origin_date"]]) +
+             lubridate::weeks(horizon) - lubridate::days(1),
+           round = round)
+
   # Remove targets with <n models
   results <- anti_join(results,
                        results |>
@@ -146,7 +96,7 @@ import_projections <- function(round = 2,
     ungroup()
 
   # Add observed data
-  obs <- import_observed(local = local)
+  obs <- import_observed(update = update)
   results <- left_join(results,
                        obs |>
                          select(obs = value,
